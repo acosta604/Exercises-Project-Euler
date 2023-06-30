b5bfd5c1d82d8dd47210a99dd1ef7e5cf5d22dc5
acosta604 <paulaacosta604@gmail.com>
2023-06-29 19:25:59 -0500

    Ejercicio 1 y 2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,18 @@
+public class Ejercicio1 {
+    /*suma entre numeros multiplos de 5 o 3 entre los numeros menores a mil*/
+
+    /**If we list all the natural numbers below $10$ that are multiples of 3 or 5,
+     *  we get $3, 5, 6$ and $9$. The sum of these multiples is 23
+     Find the sum of all the multiples of 3 or  5 below  1000 .**/
+
+    public static void main(String[] args) {
+        int suma =0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                suma += i;
+            }
+        }
+        System.out.println("La suma es "+suma);
+    }
+}
+
